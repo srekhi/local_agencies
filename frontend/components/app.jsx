21b6608e9d9d50688eccd39683dd796aaf1fb517
Sunny Rekhi <rohit.rekhi@gmail.com>
2017-07-02 12:32:23 -0700

    Add top-level app/root components and skeleton for address form

---
+++ frontend/components/app.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import AddressForm from './address_form/address_form.jsx';
+
+const App = () => (
+  <div>
+    <AddressForm />
+  </div>
+);
--- frontend/local_agencies.jsx
+++ frontend/local_agencies.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+const App = () => {
+  
+};
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Testing React set up</h1>, root);
