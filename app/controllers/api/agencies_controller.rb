733912b5fb70e37a65c54f12dd2ba28f9c5bfc59
Sunny Rekhi <rohit.rekhi@gmail.com>
2017-07-03 16:27:29 -0700

    Cont refactoring API logic

--- app/controllers/api/agencies_controller.rb
+++ app/controllers/api/agencies_controller.rb
@@ -13,10 +13,8 @@ class Api::AgenciesController < ApplicationController
 
     [address1, address2].each do |address|
       geocoding_call = "https://maps.googleapis.com/maps/api/geocode/json?address=#{address}&key=#{geocoding_key}"
-
       res = HTTParty.get(geocoding_call)
-      lat = res.parsed_response['results'][0]['geometry']['location']['lat']
-      lng = res.parsed_response['results'][0]['geometry']['location']['lng']
+      lat, lng = parse_api_response(res)
       @lat1, @lng1 = lat, lng if address == address1
       @lat2, @lng2 = lat, lng if address == address2
       places_call = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{lat},#{lng}&radius=#{TEN_MILES_IN_METERS}&type=#{SEARCH_TYPE}&key=#{places_key}"
@@ -28,8 +26,14 @@ class Api::AgenciesController < ApplicationController
 
   private
 
+  def parse_api_response(res)
+    [res.parsed_response['results'][0]['geometry']['location']['lat'],
+     res.parsed_response['results'][0]['geometry']['location']['lng']]
+  end
+
   def format_params
-    [params[:address1].split(" ").join("+"), params[:address2].split(" ").join("+")]
+    [params[:address1].split(" ").join("+"),
+     params[:address2].split(" ").join("+")]
   end
 
   def create_location_objects
@@ -44,6 +48,7 @@ class Api::AgenciesController < ApplicationController
         lat: @lat2 = @lat2.to_f * DEGREES_TO_RADIANS,
         lng: @lng2.to_f * DEGREES_TO_RADIANS
       }
+
     [point1, point2]
   end
 
@@ -53,9 +58,9 @@ class Api::AgenciesController < ApplicationController
       agency_lat = agency['geometry']['location']['lat'].to_f * DEGREES_TO_RADIANS
       agency_lng = agency['geometry']['location']['lng'].to_f * DEGREES_TO_RADIANS
       h_val = haversine(agency_lat, agency_lng, point1) + haversine(agency_lat, agency_lng, point2)
-      distance = 2 * EARTH_RADIUS * Math.asin(Math.sqrt(h_val))
-      agency['distance'] = distance
-      distance
+      agency['distance'] = 2 * EARTH_RADIUS * Math.asin(Math.sqrt(h_val))
+      # set the agency distance for display to user
+      agency['distance']
     end
   end
 
