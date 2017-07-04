db60aac613fa3d4dbb95a2bf7de95785b4d97ba4
Sunny Rekhi <rohit.rekhi@gmail.com>
2017-07-03 17:25:16 -0700

    Filter out duplicate agencies

--- frontend/components/agency_list/agency_list.jsx
+++ frontend/components/agency_list/agency_list.jsx
@@ -6,15 +6,18 @@ class AgencyList extends React.Component {
   }
 
   render(){
+    let seen = {};
     let agencies = this.props.agencies.map( (agency, index) => {
-      let rating = agency.rating ? String(agency.rating) + "/5" : 'N/A';
-      return(
-        <li key={index} className="agency-list-item">
-        <p className="agency-name">{agency.name}</p>
-        <p className="agency-info">User rating: {rating}</p>
-        <p className="agency-info">Distance: {agency['distance']} miles</p>
-      </li>);
-    });
+      if (!seen[agency.name]){
+        seen[agency.name] = true;
+        let rating = agency.rating ? String(agency.rating) + "/5" : 'N/A';
+        return(
+          <li key={index} className="agency-list-item">
+          <p className="agency-name">{agency.name}</p>
+          <p className="agency-info">User rating: {rating}</p>
+          <p className="agency-info">Distance: {agency['distance']} miles</p>
+        </li>);
+      }});
     return(
       <div>
         <p id="agency-header">Agencies:</p>
