eb6397779154f680495abff3ec765181fb43278c
Sunny Rekhi <rohit.rekhi@gmail.com>
2017-07-03 17:33:19 -0700

    Remove username uniqueness constraint

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@
 
 class User < ApplicationRecord
   validates :username, :password_digest, :session_token, presence: true
-  validates :username, uniqueness: true
+  validates :username
   validates :password, length: { minimum: 6, allow_nil: true }
 
   attr_reader :password
